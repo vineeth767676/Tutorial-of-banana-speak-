7fe4f31b7fd90a245a93705ab6f38414f33bcfe5
vineeth chowdary <vineethwebdev5@gmail.com>
2020-12-15 01:28:01 +0530

    initial

---
+++ app.js
@@ -0,0 +1,40 @@
+// Declaring the variables of input & processing & output
+
+var inputText = document.querySelector("inputBox");
+var buttonListner = document.querySelector("translateButton");
+var outputText = document.querySelector("outputBox")
+
+// Translator URL link
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+// function taking the input
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+// what if server is busy
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("ooopsss!!!! Try again after sometime")
+}
+
+// after clicking the translation button
+
+function clickHandler() {
+    var inputT = inputText.value; //taking input
+
+    // calling server for processing
+
+    fetch(getTranslationURL(inputT))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputText.innerText = translatedText; //output
+
+        })
+};
+
+buttonListner.addEventListener("click", clickHandler);
